296ba24c72b4f3e9912fa995b226b49eee1989ee
Naman <cool.naman.ajmera@gmail.com>
2021-05-07 11:11:12 +0530

    Remove from word Phrase

--- src/test/java/com/hashtableimplementation/MyLinkedHashMapTest.java
+++ src/test/java/com/hashtableimplementation/MyLinkedHashMapTest.java
@@ -23,4 +23,24 @@ public class MyLinkedHashMapTest {
         Assertions.assertEquals(3, (int) myLinkedHashMap.get("paranoid"));
     }
 
+    @Test
+    public void givenASentence_WhenWordIsRemoved_ShouldReturnNullFrequency() {
+        MyLinkedHashMap<String, Integer> myLinkedHashMap = new MyLinkedHashMap<>();
+        String paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+        String[] words = paragraph.toLowerCase().split(" ");
+
+        for (String word : words) {
+            Integer value = myLinkedHashMap.get(word);
+            if (value == null)
+                value = 1;
+            else
+                value++;
+            myLinkedHashMap.add(word, value);
+        }
+        System.out.println("Frequency before deletion : " + myLinkedHashMap.get("avoidable"));
+        myLinkedHashMap.deleteKey("avoidable");
+        System.out.println(myLinkedHashMap.toString());
+        System.out.println("Frequency after deletion : " + myLinkedHashMap.get("avoidable"));
+        Assertions.assertEquals(null, myLinkedHashMap.get("avoidable"));
+    }
 }
